a5d614baa9fe63349b046286c2518a74d898c546
Carl de Billy <carl.debilly@gmail.com>
2019-10-03 22:16:16 -0400

    Add another layer of support for slash-named devices

--- Zigbee2MqttAssistant/Startup.cs
+++ Zigbee2MqttAssistant/Startup.cs
@@ -70,7 +70,7 @@ namespace Zigbee2MqttAssistant
 			{
 				routes.MapRoute(
 					name: "default",
-					template: "{controller=Home}/{action=Index}/{id?}");
+					template: "{controller=Home}/{action=Index}/{*id}");
 			});
 		}
 	}
